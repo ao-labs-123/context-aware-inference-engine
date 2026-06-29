765a7934ac3188771439d72a347c15f91d55c1e2
⁠ao <aoooreee123@gmail.com>
2026-06-30 04:47:26 +0900

    Update lexicon_stage1.py

--- src /lexicon/lexicon_stage1.py
+++ src /lexicon/lexicon_stage1.py
@@ -1,17 +1,1 @@
-SUBJECT_RULES = {
   
-  "Psychological Verb": [
-    "think",
-    "feel",
-    "want",
-    "hope"
-  ],
-
-  "Attribution Override": [
-    "seemingly",
-    "allegedly",
-    "they say",
-    "I heard", 
-    "it is told"
-  ]
-}
