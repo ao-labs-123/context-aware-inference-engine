035bb3d7724fc2456eda21cf90c1e09c9fa084d5
⁠ao <aoooreee123@gmail.com>
2026-06-30 04:47:39 +0900

    Update stage1.py

--- src /stage1.py
+++ src /stage1.py
@@ -1,1 +1,17 @@
+SUBJECT_RULES = {
+  
+  "Psychological Verb": [
+    "think",
+    "feel",
+    "want",
+    "hope"
+  ],
 
+  "Attribution Override": [
+    "seemingly",
+    "allegedly",
+    "they say",
+    "I heard", 
+    "it is told"
+  ]
+}
